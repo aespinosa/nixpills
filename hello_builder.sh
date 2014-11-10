c2dca03fb5ac7a71284e0211e4fe5f3f6540ed97
Allan Espinosa <allan.espinosa@outlook.com>
2014-11-10 22:54:09 +0900

    Generalize the hello package's build process
    Nix pill #8 [1]
    
    [1] <http://lethalman.blogspot.jp/2014/08/nix-pill-8-generic-builders.html>

--- hello_builder.sh
+++
@@ -1,6 +0,0 @@
-export PATH="$gnutar/bin:$gcc/bin:$gnumake/bin:$coreutils/bin:$gawk/bin:$gzip/bin:$gnugrep/bin:$gnused/bin:$binutils/bin"
-tar -xzf $src
-cd hello-2.9
-./configure --prefix=$out
-make
-make install
